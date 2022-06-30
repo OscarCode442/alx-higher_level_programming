6e04d9c649448560ae1a788f445f7af7a1efd4d2
OscarCode442 <102172374+OscarCode442@users.noreply.github.com>
2022-06-30 23:32:26 +0100

    Update 4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """Print numbers 0 to 98 in decimal and hexadecimal."""
 
-for index, value  in enumerate(range(0,99)):
-    print("{} = {}".format(index, hex(value))
+for number in range(0, 99):
+    print("{} = {}".format(number, hex(number)))
